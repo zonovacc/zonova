f50fe88b6f6768faef40e4e89cd3f60e4d4b7ae3
JZamora11 <jzamora11p@gmail.com>
2020-11-14 16:12:28 -0500

    Zonova

--- js/main_nosotros.js
+++ js/main_nosotros.js
@@ -1,3 +1,31 @@
+/* PRELOADER */
+
+var progress = 0;
+document.onreadystatechange = function() {
+    if(document.readyState == "interactive") {
+        var allElement = $("*");
+        var length = allElement.length;
+        for(var i=0; i<length; i++) {
+            set_element(allElement[i], length);
+        }
+    }
+}
+
+function set_element(element, totalElement) {
+    var percetage = 100 /totalElement;
+    if ($(element).length==1) {
+        $("#fill").animate({
+            width: progress + percetage + "%"
+        }, 20, function(){
+            if(document.getElementById("fill").style.width=="100%") {
+                $(".main").fadeOut(500);
+            }
+        });
+        progress = progress + percetage;
+    }
+}
+
+
 /* MODO NOCTURNO */
 
 const btnSwitch = document.querySelector('#switch');
