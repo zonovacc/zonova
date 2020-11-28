019ad7c78e4a5cbfd2d25ec06de60c6f4137e004
JZamora11 <jzamora11p@gmail.com>
2020-11-28 16:11:35 -0500

    Zonova

--- js/main_inicio.js
+++ js/main_inicio.js
@@ -1,43 +1,8 @@
 /* PRELOADER */
 
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-  
-  (function letItSnow(){
-    var snowflakes = document.querySelectorAll('.snow');
-    for (var i = 0; i < snowflakes.length; i++) {
-      var snowflake = snowflakes[i];
-      snowflake.setAttribute('cx', getRandom(1,100) + '%');
-      snowflake.setAttribute('cy', '-' + getRandom(1,100));
-      snowflake.setAttribute('r', getRandom(1,3));
-    }
-})();
-
-var progress = 0;
-document.onreadystatechange = function() {
-    if(document.readyState == "interactive") {
-        var allElement = $("*");
-        var length = allElement.length;
-        for(var i=0; i<length; i++) {
-            set_element(allElement[i], length);
-        }
-    }
-}
-
-function set_element(element, totalElement) {
-    var percetage = 100 /totalElement;
-    if ($(element).length==1) {
-        $("#fill").animate({
-            width: progress + percetage + "%"
-        }, 10, function(){
-            if(document.getElementById("fill").style.width=="100%") {
-                $(".main").fadeOut(500);
-            }
-        });
-        progress = progress + percetage;
-    }
-}
+$(window).on("load", function(){
+    $(".loader-container").fadeOut(1500);
+});
 
 
 // POPUP VIDEO/TEASER
@@ -49,6 +14,3 @@ function toggle() {
     video.pause();
     video.currentTime = 0;
 }
-
-
-// CHATBOX
