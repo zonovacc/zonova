260aac1d97ce272c017b614740556338a8628674
JZamora11 <jzamora11p@gmail.com>
2020-11-27 19:01:58 -0500

    Zonova

--- js/main_nosotros.js
+++ js/main_nosotros.js
@@ -1,5 +1,19 @@
 /* PRELOADER */
 
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  (function letItSnow(){
+    var snowflakes = document.querySelectorAll('.snow');
+    for (var i = 0; i < snowflakes.length; i++) {
+      var snowflake = snowflakes[i];
+      snowflake.setAttribute('cx', getRandom(1,100) + '%');
+      snowflake.setAttribute('cy', '-' + getRandom(1,100));
+      snowflake.setAttribute('r', getRandom(1,3));
+    }
+})();
+
 var progress = 0;
 document.onreadystatechange = function() {
     if(document.readyState == "interactive") {
