97206fe4ee7045762afcf25e2d24699525a581d4
JZamora11 <jzamora11p@gmail.com>
2020-12-02 10:27:55 -0500

    Zonova

--- js/main_tiendas.js
+++ js/main_tiendas.js
@@ -23,6 +23,20 @@ $('.filter li').click(function(){
     return false
 });
 
+    /* RESPONSIVE */
+
+$(document).ready(function() {
+    $(".default_option").click(function() {
+        $(this).parent().toggleClass("active");
+    })
+
+    $(".select_ul li").click(function() {
+        var currentele = $(this).html();
+        $(".default_option li").html(currentele);
+        $(this).parents(".select_wrap").removeClass("active");
+    })
+});
+
 
 /* BUSCADOR */
 
