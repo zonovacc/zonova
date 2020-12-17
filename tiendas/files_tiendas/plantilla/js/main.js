e7e6258bc5b36cd7a78f79ab4cebcffb779aedd7
JZamora11 <jzamora11p@gmail.com>
2020-12-17 14:42:42 -0500

    Zonova

--- tiendas/files_tiendas/plantilla/js/main.js
+++ tiendas/files_tiendas/plantilla/js/main.js
@@ -55,6 +55,16 @@ $(document).ready(function() {
 
 /* PRODUCTOS */
 
+$(".default_option").click(function(){
+	$(this).parent().toggleClass("active");
+})
+  
+$(".select_ul li").click(function(){
+	var currentele = $(this).html();
+	$(".default_option li").html(currentele);
+	$(this).parents(".select_wrap").removeClass("active");
+})
+
 var containerEl = document.querySelector('.container');
 
 var mixer = mixitup(containerEl);
