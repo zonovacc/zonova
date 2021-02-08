587230c57e32bea914b037c2b6be626fae94ed84
JZamora11 <jzamora11p@gmail.com>
2021-02-08 12:33:51 -0500

    Zonova

---
+++ js/main_pf.js
@@ -0,0 +1,31 @@
+/* PRELOADER */
+
+$(window).on("load", function(){
+    $(".loader-container").fadeOut(1500);
+});
+
+
+/* PREGUNTAS Y RESPUESTAS */
+
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+    categoria.addEventListener('click', (e) => {
+        categorias.forEach((elemento) => {
+            elemento.classList.remove('activa');
+        });
+
+        e.currentTarget.classList.toggle('activa');
+        categoriaActiva = categoria.dataset.categoria;
+
+        contenedorPreguntas.forEach((contenedor) => {
+            if(contenedor.dataset.categoria === categoriaActiva){
+                contenedor.classList.add('activo');
+            } else {
+                contenedor.classList.remove('activo');
+            }
+        });
+    });
+});
